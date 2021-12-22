6bd33f97b389d8d8bbb8192cd76200fcef23e307
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-22 15:09:09 +0200

    refactored some properties for the Ship class

--- domain/ship.py
+++ domain/ship.py
@@ -2,24 +2,19 @@
 
 class Ship:
 
-    def __init__(self, id, size, orientation, position):
-        self.__id = id
-        self.__size = size
-        self.__orientation = orientation
+    def __init__(self, ship_id, position):
+        self.__id = ship_id
         self.__position = position
+        self.__cells = [f"ok{i}" for i in range(1, self.__position.size + 1)]
 
     @property
     def id(self):
         return self.__id
 
     @property
-    def size(self):
-        return self.__size
-
-    @property
-    def orientation(self):
-        return self.__orientation
-
-    @property
     def position(self):
         return self.__position
+
+    @property
+    def cells(self):
+        return self.__cells
