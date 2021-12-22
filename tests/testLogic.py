dbc7dfd421264e31ac45fd7a2dc7b7ed03430b22
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-22 16:51:32 +0200

    tested logic add_ship

---
+++ tests/testLogic.py
@@ -0,0 +1,21 @@
+
+import unittest
+
+from boards.board import Board
+from logic.logic import Logic
+from validation.validation import PositionValidation
+from domain.orientation import Orientation
+from domain.position import Position
+
+
+class LogicTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.board = Board()
+        self.position_validation = PositionValidation
+        self.logic = Logic(self.board, self.position_validation)
+
+    def test_add_ship(self):
+        position = Position(1, 2, 3, Orientation.Vertical)
+        self.logic.add_ship(position)
+        self.assertEqual(self.board.ships.__len__(), 1)
