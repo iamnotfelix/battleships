6d199b30a2ecd63c0272958461c7ed66e518c11b
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-23 14:30:06 +0200

    tested input validation

--- tests/testValidation.py
+++ tests/testValidation.py
@@ -1,8 +1,8 @@
 
 import unittest
 
-from validation.validation import PositionValidation
-from exceptions.exceptions import PositionException
+from validation.validation import PositionValidation, InputValidation
+from exceptions.exceptions import PositionException, InputException
 from boards.board import Board
 from domain.position import Position
 from domain.orientation import Orientation
@@ -51,3 +51,47 @@ class PositionValidationTest(unittest.TestCase):
         with self.assertRaises(PositionException) as ex:
             self.validator.validate(ship, self.board)
         self.assertEqual(str(ex.exception), "The ship does not fit there! Ship out of bounds!")
+
+
+class TestInputValidation(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.validator = InputValidation()
+
+    def test_validate_coordinate(self):
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_coordinate('b')
+        self.assertEqual(str(ex.exception), "Coordinate must be a natural number!")
+
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_coordinate('0')
+        self.assertEqual(str(ex.exception), "Coordinate out of bounds! Coordinates must be between 1 and 10!")
+
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_coordinate('11')
+        self.assertEqual(str(ex.exception), "Coordinate out of bounds! Coordinates must be between 1 and 10!")
+
+        coord = self.validator.validate_coordinate('10')
+        self.assertEqual(coord, 10)
+
+        coord = self.validator.validate_coordinate('1')
+        self.assertEqual(coord, 1)
+
+    def test_validate_orientation(self):
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_orientation('b')
+        self.assertEqual(str(ex.exception), "Orientation must be 1 or 2!")
+
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_orientation('3')
+        self.assertEqual(str(ex.exception), "Orientation must be 1 or 2!")
+
+        with self.assertRaises(InputException) as ex:
+            self.validator.validate_orientation('0')
+        self.assertEqual(str(ex.exception), "Orientation must be 1 or 2!")
+
+        orientation = self.validator.validate_orientation('1')
+        self.assertEqual(orientation, Orientation.Vertical)
+
+        orientation = self.validator.validate_orientation('2')
+        self.assertEqual(orientation, Orientation.Horizontal)
