1a892c0a37cf164bcef1c5ae0fffb39965de1f59
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-23 14:30:35 +0200

    updated logic tests

--- tests/testLogic.py
+++ tests/testLogic.py
@@ -15,6 +15,9 @@ class LogicTest(unittest.TestCase):
         self.position_validation = PositionValidation()
         self.logic = Logic(self.board, self.position_validation)
 
+    def test_properties(self):
+        self.assertEqual(self.logic.board, self.board)
+    
     def test_add_ship(self):
         position = Position(1, 2, 3, Orientation.Vertical)
         self.logic.add_ship(position)
