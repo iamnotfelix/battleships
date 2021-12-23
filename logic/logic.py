7d8c6e1dd57e69a1aff0662d1b2fc71da52215d5
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-23 19:41:43 +0200

    changed properties from private to protected

--- logic/logic.py
+++ logic/logic.py
@@ -3,35 +3,35 @@
 class Logic:
 
     def __init__(self, board, shots_board, position_validator):
-        self.__board = board
-        self.__shots_board = shots_board
-        self.__position_validator = position_validator
-        self.__id_counter = -1
+        self._board = board
+        self._shots_board = shots_board
+        self._position_validator = position_validator
+        self._id_counter = -1
 
     @property
     def board(self):
-        return self.__board
+        return self._board
 
     @property
     def shots_board(self):
-        return self.__shots_board
+        return self._shots_board
 
     def id(self):
-        self.__id_counter += 1
-        return self.__id_counter
+        self._id_counter += 1
+        return self._id_counter
 
     def add_ship(self, position):
-        ship = self.__board.create_ship(self.id(), position)
-        self.__position_validator.validate(ship, self.__board)
-        self.__board.add_ship(ship)
+        ship = self._board.create_ship(self.id(), position)
+        self._position_validator.validate(ship, self._board)
+        self._board.add_ship(ship)
 
     def add_hit(self, position):
-        is_hit, is_destroyed = self.__board.add_hit(position)
-        self.__shots_board.add_hit(position)
+        is_hit, is_destroyed = self._board.add_hit(position)
+        self._shots_board.add_hit(position)
         return is_hit, is_destroyed
 
     def is_game_over(self):
-        ships = self.__board.ships
+        ships = self._board.ships
         game_over = True
         for key in ships.keys():
             ship = ships[key]
