8f099049a09b8f7f03cdedacabfbb685c3f65af8
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-23 15:46:01 +0200

    changed some properties and updated the tests for ship

--- domain/ship.py
+++ domain/ship.py
@@ -5,7 +5,16 @@ class Ship:
     def __init__(self, ship_id, position):
         self.__id = ship_id
         self.__position = position
-        self.__cells = [f"ok{i}" for i in range(1, self.__position.size + 1)]
+        self.__cells = [False for i in range(0, self.__position.size)]
+
+    def __len__(self):
+        return len(self.__cells)
+
+    def __getitem__(self, item):
+        return self.__cells[item]
+
+    def __setitem__(self, key, value):
+        self.__cells[key] = value
 
     @property
     def id(self):
@@ -14,7 +23,3 @@ class Ship:
     @property
     def position(self):
         return self.__position
-
-    @property
-    def cells(self):
-        return self.__cells
--- tests/testShip.py
+++ tests/testShip.py
@@ -11,10 +11,27 @@ class ShipTest(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
+    def test_len(self):
+        ship = Ship(0, Position(1, 2, 3, Orientation.Vertical))
+        self.assertEqual(len(ship), 3)
+
+    def test_getitem(self):
+        ship = Ship(0, Position(1, 2, 3, Orientation.Vertical))
+        self.assertEqual(ship[0], False)
+        self.assertEqual(ship[1], False)
+        self.assertEqual(ship[2], False)
+
+    def test_setitem(self):
+        ship = Ship(0, Position(1, 2, 3, Orientation.Vertical))
+        ship[0] = True
+        ship[2] = True
+        self.assertEqual(ship[0], True)
+        self.assertEqual(ship[1], False)
+        self.assertEqual(ship[2], True)
+
     def test_properties(self):
         position = Position(1, 2, 3, Orientation.Vertical)
         ship = Ship(0, position)
 
         self.assertEqual(ship.id, 0)
         self.assertEqual(ship.position, position)
-        self.assertEqual(ship.cells, ['ok1', 'ok2', 'ok3'])
