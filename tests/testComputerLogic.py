0198c3bb4c23e692647c1a78c8a49cabe28cb696
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-24 13:54:40 +0200

    tested the strategy

--- tests/testComputerLogic.py
+++ tests/testComputerLogic.py
@@ -22,3 +22,12 @@ class ComputerLogicTest(unittest.TestCase):
     def test_init_board(self):
         self.logic.init_board()
         self.assertEqual(len(self.logic.board.ships), 5)
+
+    def test_strategy(self):
+        position = self.logic.get_new_position()
+        self.assertTrue(isinstance(position, Position))
+        self.assertTrue(self.logic.shots_board.board[position.x][position.y] != 'X')
+
+        self.logic.record_hit(position, True, False)
+        position1 = self.logic.get_new_position()
+        self.assertTrue(position1.x != position.x or position1.y != position.y)
