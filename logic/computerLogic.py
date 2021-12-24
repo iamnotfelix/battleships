8ed704d4f88ba1a6e7101af4010dd5192f7a1d9c
iamnotfelix <andrei.iliescu03@gmail.com>
2021-12-24 13:54:18 +0200

    implemented a strategy for the computer

--- logic/computerLogic.py
+++ logic/computerLogic.py
@@ -10,6 +10,7 @@ class ComputerLogic(Logic):
 
     def __init__(self, board, shots_board, position_validator):
         super().__init__(board, shots_board, position_validator)
+        self.__hit_queue = list()
 
     @staticmethod
     def generate_position(size=None, orientation=None):
@@ -32,12 +33,29 @@ class ComputerLogic(Logic):
                 except Exception as ex:
                     pass
 
+    def get_new_position(self):
+        position = None
+        if len(self.__hit_queue):
+            pair = self.__hit_queue[0]
+            position = Position(pair[0], pair[1])
+            self.__hit_queue.pop(0)
+        else:
+            is_valid = False
+            while not is_valid:
+                position = self.generate_position()
+                if self._shots_board.board[position.x][position.y] != 'X':
+                    is_valid = True
+        return position
 
-# if __name__ == "__main__":
-#     from boards.board import Board
-#     from validation.validation import PositionValidation
-#     board = Board()
-#     shots_board = Board()
-#     validator = PositionValidation()
-#     logic = ComputerLogic(board, shots_board, validator)
-#     logic.init_board()
+    def record_hit(self, position, is_hit, is_destroyed):
+        dir_x = [1, 0, -1, 0]
+        dir_y = [0, 1, 0, -1]
+        super().record_hit(position)
+        if is_hit and not is_destroyed:
+            x = position.x
+            y = position.y
+            for i in range(0, 4):
+                new_x = x + dir_x[i]
+                new_y = y + dir_y[i]
+                if 1 <= new_x <= 10 and 1 <= new_y <= 10 and self._shots_board.board[new_x][new_y] != 'X':
+                    self.__hit_queue.append((new_x, new_y))
