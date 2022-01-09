cdf7d5dbfbbe2bb5c341e97ace62438d1e570a3f
iamnotfelix <andrei.iliescu03@gmail.com>
2022-01-09 21:37:36 +0200

    splitted the GUI in multiple classes

--- gui/gui.py
+++ gui/gui.py
@@ -6,42 +6,28 @@ from domain.position import Position
 from validation.validation import PositionValidation
 
 
-class State:
-
-    def __init__(self):
-        self.dragging = False
-        self.ship_dragged = None
+class MenuScreen:
+    pass
 
 
-class GUI:
+class PlacingShipsScreen:
 
-    def __init__(self, player_logic, computer_logic):
+    def __init__(self, screen, player_logic):
         self.__player_logic = player_logic
-        self.__computer_logic = computer_logic
 
-        pygame.init()
-        pygame.display.set_caption("Battleships")
-        self.__screen = pygame.display.set_mode((1280, 680), pygame.SCALED)
+        self.__screen = screen
         self.__ships = list()
         self.__sprites = dict()
-        self.__texts = dict()
 
         """ constants """
         self.BOARD_X = 200
         self.BOARD_Y = 90
         self.HEADER_X = 300
         self.HEADER_Y = 12
-        self.FOOTER_X = 135  # 235
-        self.FOOTER_Y = 600
+        self.PRESS_X = 100
+        self.PRESS_Y = 600
         self.CONTENT_X = 820
         self.CONTENT_Y = 12
-        self.FINISH_X = 680
-        self.FINISH_Y = 600
-
-        """ Screen state"""
-        self.is_menu = False
-        self.is_placing_ships = True
-        self.is_playing = False
 
         """ States & active assets """
         self.loop = True
@@ -52,15 +38,7 @@ class GUI:
     def __update_window(self):
         for key in self.__sprites.keys():
             self.__screen.blit(self.__sprites[key].surface, self.__sprites[key].position)
-        for key in self.__texts.keys():
-            self.__screen.blit(self.__texts[key].surface, self.__texts[key].position)  # (200, 12)
         pygame.display.update()
-
-    def __check_placed_ships(self):
-        all_placed = True
-        for ship in self.__ships:
-            all_placed = all_placed and ship.placed
-        return all_placed
 
     def __load_assets(self):
         background = GameObject("data/background.png", (0, 0))
@@ -71,30 +49,34 @@ class GUI:
         ship_32 = GameObject("data/3.png", (820, 390), 3)
         ship_2 = GameObject("data/2.png", (820, 490), 2)
 
-        font = pygame.font.SysFont('agencyfb', 60, False, False)
-        header_text = TextObject(font, "Place your fleet", (self.HEADER_X, self.HEADER_Y))
-        footer_text = TextObject(font, "Press 'R' to rotate ship", (self.FOOTER_X, self.FOOTER_Y))
+        font_header = pygame.font.SysFont('agencyfb', 60, False, False)
+        font = pygame.font.SysFont("agencyfb", 50, False, False)
+        header_text = TextObject(font_header, "Place your fleet", (self.HEADER_X, self.HEADER_Y))
+        press_text = TextObject(font, "'R' - to rotate ship    'Enter' - to start playing     'Esc' - to go to menu",
+                                (self.PRESS_X, self.PRESS_Y))
         content_text = TextObject(font, "Your ships:", (self.CONTENT_X, self.CONTENT_Y))
-        finish_text = TextObject(font, "Press 'Enter' when done", (self.FINISH_X, self.FINISH_Y))
 
-        self.__texts = {
-            "header": header_text,
-            "footer": footer_text,
-            "content": content_text,
-            "finish": finish_text
-        }
         self.__sprites = {
-            "background" : background,
+            "background": background,
             "board": board,
             "ship_5": ship_5,
             "ship_4": ship_4,
             "ship_31": ship_31,
             "ship_32": ship_32,
-            "ship_2": ship_2
+            "ship_2": ship_2,
+            "header": header_text,
+            "press": press_text,
+            "content": content_text
         }
         self.__ships = [ship_5, ship_4, ship_31, ship_32, ship_2]
 
-    def __ship_placing_screen(self, event):
+    def __check_placed_ships(self):
+        all_placed = True
+        for ship in self.__ships:
+            all_placed = all_placed and ship.placed
+        return all_placed
+
+    def __event_handler(self, event):
         if event.type == pygame.QUIT:
             self.loop = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -132,30 +114,113 @@ class GUI:
                         for ship in self.__ships:
                             if not (ship in self.added_ships):
                                 ship_pos = ship.position
-                                ship_pos = (
-                                (ship_pos[0] - self.BOARD_X) // 50 + 1, (ship_pos[1] - self.BOARD_Y) // 50 + 1)
+                                ship_pos = ((ship_pos[0] - self.BOARD_X) // 50 + 1, (ship_pos[1] - self.BOARD_Y) // 50 + 1)
                                 ship_pos = Position(ship_pos[1], ship_pos[0], ship.size, ship.orientation)
                                 self.__player_logic.add_ship(ship_pos)
                                 self.added_ships.append(ship)
+                        return self.__ships
+                        # todo: go to next screen
                     except Exception as ex:
                         print(str(ex))
-                        print("Not all ships are placed correctly!")
-                    print("all")
+                        # todo: create a pop up message for error
                 else:
+                    # todo: create a pop up message for error
                     print("Not all ships are placed correctly!")
         self.__update_window()
+        return None
 
     def start(self):
         self.__load_assets()
-
         while self.loop:
             for event in pygame.event.get():
-                if self.is_menu:
+                ships = self.__event_handler(event)
+                if isinstance(ships, list):
+                    return ships
+                elif ships:
                     pass
-                elif self.is_placing_ships:
-                    self.__ship_placing_screen(event)
-                elif self.is_playing:
-                    pass
+                    # todo: go to menu option
+
+
+class PlayingScreen:
+    def __init__(self, screen, ships, player_logic):
+        self.__player_logic = player_logic
+
+        self.__screen = screen
+        self.__ships = ships
+        self.__sprites = dict()
+
+        """ constants """
+        self.BOARD_X = 200
+        self.BOARD_Y = 90
+        self.SHOTS_BOARD_X = 100
+        self.SHOTS_BOARD_Y = 100
+        self.HEADER1_X = 300
+        self.HEADER1_Y = 12
+        self.HEADER2_X = 12
+        self.HEADER2_Y = 12
+        self.PRESS_X = 100
+        self.PRESS_Y = 600
+
+        """ States & active assets """
+        self.loop = True
+
+    def __update_window(self):
+        for key in self.__sprites.keys():
+            self.__screen.blit(self.__sprites[key].surface, self.__sprites[key].position)
+        pygame.display.update()
+
+    def __load_assets(self):
+        background = GameObject("data/background.png", (0, 0))
+        board = GameObject("data/board.png", (self.BOARD_X, self.BOARD_Y))
+        shots_board = GameObject("data/board.png", (self.SHOTS_BOARD_X, self.SHOTS_BOARD_Y))
+
+        font_header = pygame.font.SysFont('agencyfb', 60, False, False)
+        font = pygame.font.SysFont("agencyfb", 50, False, False)
+        header1_text = TextObject(font_header, "Your fleet", (self.HEADER1_X, self.HEADER1_Y))
+        header2_text = TextObject(font_header, "Your shots", (self.HEADER2_X, self.HEADER2_Y))
+        press_text = TextObject(font, "'Esc' - to go to menu", (self.PRESS_X, self.PRESS_Y))
+
+        self.__sprites = {
+            "background": background,
+            "board": board,
+            "shots_board": shots_board,
+            "ship_5": self.__ships[0],
+            "ship_4": self.__ships[1],
+            "ship_31": self.__ships[2],
+            "ship_32": self.__ships[3],
+            "ship_2": self.__ships[4],
+            "header1": header1_text,
+            "header2": header2_text,
+            "press": press_text
+        }
+
+
+class GUI:
+
+    def __init__(self, player_logic, computer_logic):
+        self.__player_logic = player_logic
+        self.__computer_logic = computer_logic
+
+        pygame.init()
+        pygame.display.set_caption("Battleships")
+        self.__screen = pygame.display.set_mode((1280, 680), pygame.SCALED)
+
+        """ Screen state"""
+        self.is_menu = False
+        self.is_placing_ships = True
+        self.is_playing = False
+
+    def start(self):
+        placing_screen = PlacingShipsScreen(self.__screen, self.__player_logic)
+        placing_screen.start()
+        # while self.loop:
+        #     for event in pygame.event.get():
+        #         if self.is_menu:
+        #             pass
+        #         elif self.is_placing_ships:
+        #             self.__ship_placing_screen(event)
+        #         elif self.is_playing:
+        #             pass
 
 
 if __name__ == "__main__":
